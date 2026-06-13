57a314871dae7ac1382cf8cccc8b9adf0d03e715
boroldoi26 <62096740+boroldoi26@users.noreply.github.com>
2026-06-13 08:20:29 +0800

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,2 +1,2 @@
 // Current working Apps Script Web App URL. It must end with /exec.
-window.DASHBOARD_API_URL = 'https://script.google.com/macros/s/AKfycbzVgH-sdQGURDuvBll5kiCCsAbI35yxGkBe9_HflN6Kq-A7UvB_D7Alt2N8RefKBLm6qQ/exec';
+window.DASHBOARD_API_URL = 'https://script.google.com/macros/s/AKfycbzIADbbyGELb_y2TwWrLjnf5cRWvsfJbGRAuDLljiltAPe-R1DFF_8J1axf4_AzROtLGw/exec';
